5f132394d7e5ed8b450e71340bf499dbb53d0868
P1X3LD3M0N <77390209+P1X3LD3M0N@users.noreply.github.com>
2021-01-21 21:36:37 +0530

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
 
+void intro()
+{
+	cout<<"3𝘼 𝙎𝙏𝙍𝙀𝘼𝙈𝙄𝙉𝙂"<<endl;
+}
+
+class show
+{
+};
+
+class movie
+{
+};
+
+class genre
+{
+};
+
+int main()
+{
+	intro();
+	return 0;
+}
