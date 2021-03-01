d4432ce3ca4aa69403bd423608d5d6fb7be7b610
P1X3LD3M0N <77390209+P1X3LD3M0N@users.noreply.github.com>
2021-03-01 16:38:24 +0530

    Update fileFuncs.h

--- fileFuncs.h
+++ fileFuncs.h
@@ -1,5 +1,5 @@
 #include <fstream>
-
+using namespace std;
 void searchM1()
 {
     string movie;
@@ -16,13 +16,11 @@ void searchM2()
 {
     string movie;
     ifstream fin;
-    fin.open("movies.txt");
+    fin.open("actionM.txt");
         while (!fin.eof())
         {
             getline(fin, movie);
             cout << movie << endl;
-            if (movie == "Life of Pi")
-                    exit(0);
         }
     fin.close();
 }
@@ -30,16 +28,11 @@ void searchM3()
 {
     string movie;
     ifstream fin;
-    fin.open("movies.txt");
+    fin.open("comedyM.txt");
         while (!fin.eof())
         {
             getline(fin, movie);
-            while (movie != "Home Alone")
-            {
-            }
             cout << movie << endl;
-            if (movie == "The Smurfs")
-                    exit(0);
         }
     fin.close();
 }
@@ -47,13 +40,10 @@ void searchM4()
 {
     string movie;
     ifstream fin;
-    fin.open("movies.txt");
+    fin.open("dramaS.txt");
         while (!fin.eof())
         {
             getline(fin, movie);
-            while (movie != "To All The Boys I Ever Loved")
-            {
-            }
             cout << movie << endl;
         }
     fin.close();
@@ -74,13 +64,11 @@ void searchS2()
 {
     string show;
     ifstream fin;
-    fin.open("shows.txt");
+    fin.open("actionS.txt");
         while (!fin.eof())
         {
             getline(fin, show);
             cout << show << endl;
-            if (show == "Black Mirror")
-                    exit(0);
         }
     fin.close();
 }
@@ -88,16 +76,11 @@ void searchS3()
 {
     string show;
     ifstream fin;
-    fin.open("shows.txt");
+    fin.open("comedyS.txt");
         while (!fin.eof())
         {
             getline(fin, show);
-            while (show != "Brooklyn Nine-Nine")
-            {
-            }
             cout << show << endl;
-            if (show == "The Mordern Family")
-                    exit(0);
         }
     fin.close();
 }
@@ -105,15 +88,139 @@ void searchS4()
 {
     string show;
     ifstream fin;
-    fin.open("shows.txt");
+    fin.open("dramaS.txt");
         while (!fin.eof())
         {
             getline(fin, show);
-            while (show != "Grey's Anatomy")
-            {
-            }
             cout << show << endl;
         }
     fin.close();
 }
-
+void watchS()
+{
+    string show, check;
+    int flag;
+    ifstream fin;
+    fin.open("shows.txt");
+    cout << "Which show would you like to see?\n";
+    cin >> check;
+    while (!fin.eof())
+    {
+        getline(fin, show);
+        if (show == check)
+        {
+            flag = 1;
+        }
+    }
+    fin.close();
+    if (flag == 1)
+    {
+        cout << "You are currently watching " << check << ". Please enjoy!\n\nThank you for watching!";
+    }
+}
+void watchM()
+{
+    string movie, check;
+    int flag;
+    ifstream fin;
+    fin.open("movies.txt");
+    cout << "Which movie would you like to see?\n";
+    cin >> check;
+    while (!fin.eof())
+    {
+        getline(fin, movie);
+        if (movie == check)
+        {
+            flag = 1;
+        }
+    }
+    fin.close();
+    if (flag == 1)
+    {
+        cout << "You are currently watching " << check << ". Please enjoy!\n\nThank you for watching!";
+    }
+}
+void addS1()
+{
+    string show;
+    ofstream fout;
+    fout.open("actionS.txt", ios::app);
+    cout << "Enter the name of the show to be added\n";
+    cin >> show;
+    fout << show;
+    cout << "Thank you!\n";
+    fout.close();
+    fout.open("shows.txt",ios::app);
+    fout << show;
+    fout.close();
+}
+void addS2()
+{
+    string show;
+    ofstream fout;
+    fout.open("comedyS.txt",ios::app);
+    cout << "Enter the name of the show to be added\n";
+    cin >> show;
+    fout << show;
+    cout << "Thank you!\n";
+    fout.close();
+    fout.open("shows.txt",ios::app);
+    fout << show;
+    fout.close();
+}
+void addS3()
+{
+    string show;
+    ofstream fout;
+    fout.open("dramaS.txt",ios::app);
+    cout << "Enter the name of the show to be added\n";
+    cin >> show;
+    fout << show;
+    cout << "Thank you!\n";
+    fout.close();
+    fout.open("shows.txt",ios::app);
+    fout << show;
+    fout.close();
+}
+void addM1()
+{
+    string movie;
+    ofstream fout;
+    fout.open("actionM.txt",ios::app);
+    cout << "Enter the name of the show to be added\n";
+    cin >> movie;
+    fout << movie;
+    cout << "Thank you!\n";
+    fout.close();
+    fout.open("shows.txt",ios::app);
+    fout << movie;
+    fout.close();
+}
+void addM2()
+{
+    string movie;
+    ofstream fout;
+    fout.open("comedyM.txt",ios::app);
+    cout << "Enter the name of the show to be added\n";
+    cin >> movie;
+    fout << movie;
+    cout << "Thank you!\n";
+    fout.close();
+    fout.open("shows.txt",ios::app);
+    fout << movie;
+    fout.close();
+}
+void addM3()
+{
+    string movie;
+    ofstream fout;
+    fout.open("dramaM.txt",ios::app);
+    cout << "Enter the name of the show to be added\n";
+    cin >> movie;
+    fout << movie;
+    cout << "Thank you!\n";
+    fout.close();
+    fout.open("shows.txt",ios::app);
+    fout << movie;
+    fout.close();
+}
